0ea6d9ba5d6d1f2b480edf25fbe8e06a5a134ffc
zoooord <69426529+zoooord@users.noreply.github.com>
2020-08-09 11:49:07 -0300

    Add files via upload

---
+++ scripts/addField.js
@@ -0,0 +1,15 @@
+
+document.querySelector("#add-time")
+.addEventListener('click', cloneField)
+function cloneField() {
+    const newFieldContainer = document.querySelector('.schedule-item').cloneNode(true) //boolean: true ou false
+    
+    const fields = newFieldContainer.querySelectorAll('input')
+    
+    fields.forEach(function(field) {
+       field.value = ""
+    })
+   
+    document.querySelector('#schedule-items').appendChild(newFieldContainer)
+
+    }
